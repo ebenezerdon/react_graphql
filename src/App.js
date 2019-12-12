94f0bf927124a4fb5de4164f113633cc0d71cc50
Ebenezer Don <ebenezerdonu@gmail.com>
2019-12-12 03:06:03 +0100

    configure graphql query

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 import logo from './logo.svg';
 import './App.css';
 
+const getAllArticles = gql`{
+  articles {
+    title
+    description
+    body
+    author {
+      name
+      country
+    }
+  }
+}`
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Query query={getAllArticles}>
+        {({ loading, error, data }) => {
+          if (loading) return <p>Good things take time....</p>
+          if (error) return <p>Something went wrong...</p>
+          console.log('=====================')
+          console.log(data)
+          console.log('=====================')
+
+          return <div className="row"><ul>{data.articles.map(article => <ol>{article.title}</ol>)}</ul></div>
+        }}
+      </Query>
+
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const client = new ApolloClient({
+  uri: 'https://awesome-node-graphql.herokuapp.com/graphql'
+})
+
 ReactDOM.render(
-  <ApolloProvider>
+  <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
   document.getElementById('root')
