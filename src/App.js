023fe41b712446a52a5148d9567d555d50f7f45d
Ebenezer Don <ebenezerdonu@gmail.com>
2019-12-13 23:13:14 +0100

    remove redundant React fragment

--- src/App.js
+++ src/App.js
@@ -16,38 +16,36 @@ const getAllArticles = gql`{
 
 const App = () => {
   return (
-    <>
-      <Query query={getAllArticles}>
-        {({ loading, error, data }) => {
-          if (loading) return <p>Relax, it's worth the wait...</p>
-          if (error) return <p>Looks like we've got a problem...</p>
-          return (
-            <div className="container">
-              <h1>Articles</h1>
-              <div className="row">
-                {data.articles.map(article => (
-                  <div className="col-sm">
-                    <div className="card" style={{width: "18rem"}}>
-                      <img
-                        src={article.coverImageUrl}
-                        className="card-img-top"
-                        style={{height: "10em"}}
-                        alt="cover"
-                      />
-                      <div className="card-body">
-                        <h5 className="card-title">{article.title}</h5>
-                        <p className="card-text">{article.description}</p>
-                        <button className="btn btn-primary">Read</button>
-                      </div>
+    <Query query={getAllArticles}>
+      {({ loading, error, data }) => {
+        if (loading) return <p>Relax, it's worth the wait...</p>
+        if (error) return <p>Looks like we've got a problem...</p>
+        return (
+          <div className="container">
+            <h1>Articles</h1>
+            <div className="row">
+              {data.articles.map(article => (
+                <div className="col-sm">
+                  <div className="card" style={{width: "18rem"}}>
+                    <img
+                      src={article.coverImageUrl}
+                      className="card-img-top"
+                      style={{height: "10em"}}
+                      alt="cover"
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{article.title}</h5>
+                      <p className="card-text">{article.description}</p>
+                      <button className="btn btn-primary">Read</button>
                     </div>
                   </div>
-                ))}
-              </div>
+                </div>
+              ))}
             </div>
-          )
-        }}
-      </Query>
-    </>
+          </div>
+        )
+      }}
+    </Query>
   );
 }
 
