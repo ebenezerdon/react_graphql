30af840a81f572db097690847e1174d274257adf
Ebenezer Don <ebenezerdonu@gmail.com>
2019-12-13 14:37:10 +0100

    revert url to https;

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const client = new ApolloClient({
-  uri: 'wss://awesome-node-graphql.herokuapp.com/graphql'
+  uri: 'https://awesome-node-graphql.herokuapp.com/graphql'
 })
 
 ReactDOM.render(
